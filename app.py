af011ba43fafc3a976459fd25ebfe71cf05da7b4
mitanshu610 <mitubhatt670@gmail.com>
2019-12-29 20:03:24 +0530

    Custom Search API added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from googleapiclient.discovery import build
+import pprint
 
 app = Flask(__name__)
 
@@ -8,10 +10,16 @@ def hello_world():
     return render_template('index.html')
 
 
+api_key = "AIzaSyBAIubVGkjrcUFeAG-bcYZhuUXCgj_bx00"
+cse_id = "015650286644973668229:h8ykdkmq6gu"
+
+
 @app.route('/', methods=['POST'])
 def data_process():
     text = request.form['text']
-    print(text)
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=text, cx=cse_id, num=10).execute()
+    print(res['url']['items']['snippet'])
     return text
 
 if __name__ == '__main__':
